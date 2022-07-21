b01889250534a3b5fd82a9031a56dc352c56aeb6
Pedro Henrique <p.nascimento.santos@avanade.com>
2022-07-21 15:33:08 -0300

    anime

--- demo/src/main/java/com/example/demo/controller/ControlAnime.java
+++ demo/src/main/java/com/example/demo/controller/ControlAnime.java
@@ -1,2 +1,29 @@
-package com.example.demo.controller;public class ControlAnime {
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+import  org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import  com.example.demo.*;
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
+import java.util.List;
+
+
+@RestController
+public class ControlAnime {
+    @Autowired
+    AnimeDAO animeDAO;
+AnimeRepository animeRepository;
+@GetMapping("/anime")
+    List<Anime>all(){
+     return null;
 }
+
+@PostMapping("/aa")
+public int ss() {
+ return 0;
+}
+
+
+
+}
+
