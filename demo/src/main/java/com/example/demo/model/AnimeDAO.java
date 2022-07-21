f5e06985bf98c10da8b22bbda759889422f8a789
Pedro Henrique <p.nascimento.santos@avanade.com>
2022-07-21 15:31:49 -0300

    DAO

--- demo/src/main/java/com/example/demo/model/AnimeDAO.java
+++ demo/src/main/java/com/example/demo/model/AnimeDAO.java
@@ -1,6 +1,21 @@
 package com.example.demo.model;
+import org.springframework.data.annotation.Id;
 
-public class AnimeDAO {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class AnimeDAO extends Serializable {
+
+    @Id
+    int animeId;///
+
+
 
 
 
